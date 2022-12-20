2bac1a7508f1c1fb331f31eb768ecd41558d302f
MaryanaAm <marydeaddoll@gmail.com>
2022-12-20 19:11:51 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите длину массива: ");
+int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[lengthOfArray];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите значение эдемента {i} : ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write($"Вывод массива: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+        Console.Write($"{array[i]} ");
+        
+}
+
+int count = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if(array[j] > 0)
+     count++;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Количество положительных чисел {count}: ");
---
+++ 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// y = k1 * x + b1, y = k2 * x + b2
+// k1 * x + b1 = k2 * x + b2
+// (k1 - k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
+
+(double, double)GetValue(double b1, double k1, double b2, double k2)
+{
+    double x = (b2-b1) / (k1-k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+(double x, double y) = GetValue(4, 12, 8, 4);
+Console.Write($"X: {x}, Y: {y}");
+
